9b015ae54a538535c8b96ffd69be11d3cca22d5e
tarik2503 <tariksk3325@gmail.com>
2026-07-17 02:14:59 +0530

    Did changes of DBContext

---
+++ API/Controllers/MembersController.cs
@@ -0,0 +1,12 @@
+using API.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MembersController(AppDbContext context) : ControllerBase
+    {
+    }
+}
