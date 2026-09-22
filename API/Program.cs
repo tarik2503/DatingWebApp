ba8424a462e903e6fc1722117b7b2027f9aabd0b
tarik2503 <tariksk3325@gmail.com>
2026-09-23 00:06:14 +0530

    Designed the member-list skeleton

--- API/Program.cs
+++ API/Program.cs
@@ -4,17 +4,19 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 builder.Services.AddDbContext<AppDbContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+// CORS policy
+builder.Services.AddCors();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200","https://localhost:4200"));
 app.MapControllers();
 
 app.Run();
--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,12 +1,30 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit, signal} from '@angular/core';
+import { lastValueFrom } from 'rxjs/internal/lastValueFrom';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = signal('client');
+export class App implements OnInit{
+  private http = inject(HttpClient);
+  protected title = 'Dating App';
+  protected members = signal<any>([])
+
+  async ngOnInit() {
+    this.members.set(await this.getMembers())
+  }
+
+  async getMembers(){
+    try{
+      return lastValueFrom(this.http.get('https://localhost:5001/api/members'));
+    }
+    catch(error){
+      console.error('Error fetching members:', error);
+      throw error;
+    }
+  }
+
 }
